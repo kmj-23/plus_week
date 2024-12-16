0afa213de0132537a874f3b0ecf50863bc2a8bd7
kmj <kmj-23>
2024-12-16 17:20:15 +0900

    feat/admin interceptor

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -10,6 +10,7 @@ public class AdminController {
     private final AdminService adminService;
 
     public AdminController(AdminService adminService) {
+
         this.adminService = adminService;
     }
 
